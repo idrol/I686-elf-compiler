7ccf1a1800b948e11725d4c57e8dbd7036774e2a
idrol <adriangrannas@hotmail.com>
2024-09-02 17:14:08 +0300

    Added missing dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-apt-get install -y build-essential grub-common xorriso bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo libisl-dev gcc
+apt update
+apt-get install -y build-essential grub-common xorriso bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo libisl-dev gcc cmake ninja-build gdb
