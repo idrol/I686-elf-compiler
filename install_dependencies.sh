5c271973e3cb4e38793db62877c08d160ebf771c
idrol <adriangrannas@hotmail.com>
2024-09-03 18:00:34 +0300

    Added grub2 dependency

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 apt update
-apt-get install -y build-essential grub-common xorriso bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo libisl-dev gcc cmake ninja-build gdb dosfstools
+apt-get install -y build-essential grub-common xorriso bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo libisl-dev gcc cmake ninja-build gdb dosfstools grub2
